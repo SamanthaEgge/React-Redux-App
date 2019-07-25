97677378f985534c44f2b94ce16e576c5fb12b9f
unknown <samantha.c.egge@gmail.com>
2019-07-25 14:35:07 -0700

    Refactored some of the code. Now correctly fetching data from the api. Need to display to page.

--- pokemon/src/components/PokemonList/PokemonList.js
+++ pokemon/src/components/PokemonList/PokemonList.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-const PokemonList = () => {
+import './PokemonList.scss'
+
+const PokemonList = (props) => {
+
+  const initPokemon = () => {
+    props.fetchInitPokemon()
+  }
+
+  // if (props.isFetching) {
+  //   return <h1>Loading . . .</h1>;
+  // }
+
+  useEffect(() => {
+    initPokemon()
+    console.log(props.pokemonList)
+  },[])
+
   return(
     <div className='pokemon-list'>
       Pokemon list
